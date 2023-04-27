f160e532ac15dea7ff84cfbf5a35d0ae2bcdf08a
Tarazonaa <andres.tara.so@gmail.com>
2023-04-27 11:49:56 -0600

    Camera breakpoint. Delete flying_goku sprite as child object

--- Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
+++ Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
@@ -6,9 +6,14 @@ public class cameraMovement : MonoBehaviour
 {
     [SerializeField] Transform player;
 
+    // Set Camera move breakpoint
+    [SerializeField] float minlength;
+
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y,  2) >= Mathf.Pow(minlength, 2)) {
+            transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / 144, transform.position.y + (player.position.y - transform.position.y) / 10, transform.position.z);
+        }
     }
 }
