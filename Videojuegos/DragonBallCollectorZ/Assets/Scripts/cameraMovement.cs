e357814690446181c85fc1246da4f2a62e863518
Tarazonaa <andres.tara.so@gmail.com>
2023-04-27 12:15:30 -0600

    Camera smooth tings

--- Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
+++ Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
@@ -8,13 +8,14 @@ public class cameraMovement : MonoBehaviour
 
     // Set Camera move breakpoint
     [SerializeField] float minX;
-    [SerializeField] float minY;
+    float minY;
 
     // Update is called once per frame
     void Update()
     {
+        minY = minX * 9/16;
         if (Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y,  2) >= (Mathf.Pow(minX, 2) + Mathf.Pow(minY, 2))) {
-            transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / 144, transform.position.y + (player.position.y - transform.position.y) / 10, transform.position.z);
+            transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / 144, transform.position.y + (player.position.y - transform.position.y) / 144, transform.position.z);
         }
     }
 }
