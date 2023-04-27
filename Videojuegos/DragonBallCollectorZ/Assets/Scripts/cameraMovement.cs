33c1e83e3b2feea7057aa729b3350bec03fa8962
Tarazonaa <andres.tara.so@gmail.com>
2023-04-27 12:00:31 -0600

    Changed minlength for minx and miny

--- Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
+++ Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
@@ -7,12 +7,13 @@ public class cameraMovement : MonoBehaviour
     [SerializeField] Transform player;
 
     // Set Camera move breakpoint
-    [SerializeField] float minlength;
+    [SerializeField] float minX;
+    [SerializeField] float minY;
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y,  2) >= Mathf.Pow(minlength, 2)) {
+        if (Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y,  2) >= (Mathf.Pow(minX, 2) + Mathf.Pow(minY, 2))) {
             transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / 144, transform.position.y + (player.position.y - transform.position.y) / 10, transform.position.z);
         }
     }
