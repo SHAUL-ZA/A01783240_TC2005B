7154c5803769c42024272dc60cd99ab4ed2b4676
Tarazonaa <andres.tara.so@gmail.com>
2023-04-27 12:50:42 -0600

    Make camera change due to resolution and change limits based off aspect ratio

--- Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
+++ Videojuegos/DragonBallCollectorZ/Assets/Scripts/cameraMovement.cs
@@ -10,13 +10,26 @@ public class cameraMovement : MonoBehaviour
     [SerializeField] float minX;
     float minY;
 
+    // Declare camera 
+    Camera cam;
+
+    // Set camera refresh rate 
+    float refreshRate;
+
+
+    void Start() {
+        cam = GetComponent<Camera>();
+        // Get refresh rate based on screen
+        refreshRate =  Screen.currentResolution.refreshRate;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        minY = minX * 9/16;
+        minY = minX * cam.aspect;
         float time = Time.deltaTime;
         if (Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y,  2) >= (Mathf.Pow(minX, 2) + Mathf.Pow(minY, 2))) {
-            transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / 60, transform.position.y + (player.position.y - transform.position.y) / 60, transform.position.z);
+            transform.position = new Vector3(transform.position.x + (player.position.x - transform.position.x) / (refreshRate / 2), transform.position.y + (player.position.y - transform.position.y) / (refreshRate / 2), transform.position.z);
         }
     }
 }
