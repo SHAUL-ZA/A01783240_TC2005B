77b0f15367557d509dda0049bcbb31c6bb33b597
Shaul Zayat <shaulzayat1@gmail.com>
2023-04-28 01:53:07 -0600

    Add DragonBalls / Winning Scene

---
+++ Videojuegos/DragonBallCollectorZ/Assets/Scripts/sphereCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class sphereCollector : MonoBehaviour
+{
+    int counter = 0;
+    [SerializeField] TMP_Text spheres;
+
+    void OnTriggerEnter2D(Collider2D colision){
+        if(colision.gameObject.CompareTag("DragonBall")){
+            Destroy(colision.gameObject);
+            counter++;
+            spheres.text = "Score: " + counter;
+        }
+    }
+
+    void Update(){//Si el jugador colecta todas las esferas
+        if(counter == 7){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //Carga escena de "Haz ganado!!"
+        }
+    }
+}
+
